0a0121742ed6dba807ad3fb3de01ad3e7f75a1e6
Abolfazl M <abolfazl.mk.2002@gmail.com>
2021-11-12 15:49:25 +0330

    compelte templates

--- accounting/accountents/urls.py
+++ accounting/accountents/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('',views.Accountents_HomePage_View.as_view(),name="accountents_home"),
     path('<int:pk>',views.Accountents_InfoPage_View.as_view(),name="accountents_info"),
     path('add/',views.Accountents_AddPage_View.as_view(),name="accountetns_add"),
-    path('delete/<int:pk>',views.Accountents_DeletPage_View.as_view(),name="accountetns_delete"),
-    path('edit/<int:pk>',views.Accountents_EditPage_View.as_view(),name="accountetns_edit"),
+    path('<int:pk>/delete',views.Accountents_DeletPage_View.as_view(),name="accountetns_delete"),
+    path('<int:pk>/edit',views.Accountents_EditPage_View.as_view(),name="accountetns_edit"),
 ]
