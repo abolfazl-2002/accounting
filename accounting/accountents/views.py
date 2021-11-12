721c32937496ff963aa58e20ca6ee61c324da77f
Abolfazl M <abolfazl.mk.2002@gmail.com>
2021-11-12 15:52:07 +0330

    complete the views

--- accounting/accountents/views.py
+++ accounting/accountents/views.py
@@ -6,28 +6,28 @@ from django.views.generic import (
 from django.urls import reverse
 
 # Create your views here.
-
+url = "accountents/"
 class Accountents_HomePage_View(ListView):
     model = AccountryTable
-    template_name = ""
+    template_name = url + "home.html"
     context_object_name = "object"
 
 class Accountents_InfoPage_View(DetailView):
     model = AccountryTable
-    template_name = ""
+    template_name = url + "detail.html"
     context_object_name = "object"
 
 class Accountents_AddPage_View(CreateView):
     model = AccountryTable
-    template_name = ""
+    template_name = url + "new.html"
     fields = ["title","description","price","group"]
 
 class Accountents_EditPage_View(UpdateView):
     model = AccountryTable
-    template_name = ""
+    template_name = url + "edit.html"
     fields = ["title","description","price","group"]
 
 class Accountents_DeletPage_View(DeleteView):
     model = AccountryTable
-    template_name = ""
+    template_name = url + "delete.html"
     success_url = reverse()
